cb0a7fb53515c6943c937f90f2eb746a4226a71b
praveenreddy160 <praveenreddy160@gmail.com>
2022-08-26 07:54:22 +0530

    Added program for both solution

--- program1.py
+++ program1.py
@@ -4,45 +4,46 @@ input_file = "w_data.dat"
 
 def read_weather_data(input_file):
 
-	with open(input_file, "r") as fb_obj:
-		data = fb_obj.readlines()
-	return data
+    with open(input_file, "r") as fb_obj:
+        data = fb_obj.readlines()
+    return data
+
 
 def extract_cols(row):
-	rows = row.split(" ")
-	if len(rows) < 10:
-		return False
-	records = []
-	index = 0 
-	while len(records) < 3:
-		try:
-			value = rows[index].strip()
-			index+=1
-			if len(value) and int(value):
-				records.append(int(value))
-		except Exception as e:
-			break
-	return records if len(records) == 3 else []
+    rows = row.split(" ")
+    if len(rows) < 10:
+        return False
+    records = []
+    index = 0
+    while len(records) < 3:
+        try:
+            value = rows[index].strip()
+            index += 1
+            if len(value) and int(value):
+                records.append(int(value))
+        except Exception as e:
+            break
+    return records if len(records) == 3 else []
 
 
 def calculate_max_diff(data_list):
-	max_diff_record = [0, 0]
-	for row in data_list:
-		cols = extract_cols(row)
-		if not cols:
-			continue
-		diff = cols[1] - cols[2]
-		if diff > max_diff_record[0]:
-			max_diff_record[0] = diff
-			max_diff_record[1] = cols
-	return max_diff_record
+    max_diff_record = [0, 0]
+    for row in data_list:
+        cols = extract_cols(row)
+        if not cols:
+            continue
+        diff = cols[1] - cols[2]
+        if diff > max_diff_record[0]:
+            max_diff_record[0] = diff
+            max_diff_record[1] = cols
+    return max_diff_record
 
 
 if __name__ == "__main__":
 
-	print(f"Reading the input file {input_file}")
-	data = read_weather_data(input_file)
-	max_diff_record = calculate_max_diff(data)
-	print(max_diff_record)
-	print(f"Day number with max temperature spread is {max_diff_record[0]} and Max and Min temperature are {max_diff_record[1][1]} and {max_diff_record[1][2]}")
-
+    print(f"Reading the input file {input_file}")
+    data = read_weather_data(input_file)
+    max_diff_record = calculate_max_diff(data)
+    print(max_diff_record)
+    print(
+        f"Day number with max temperature spread is {max_diff_record[0]} and Max and Min temperature are {max_diff_record[1][1]} and {max_diff_record[1][2]}")
--- program2.py
+++ program2.py
@@ -1,49 +1,53 @@
 input_file = "soccer.dat"
 
+
 def read_soccer_data(input_file):
 
-	with open(input_file, "r") as fb_obj:
-		data = fb_obj.readlines()
-	return data
+    with open(input_file, "r") as fb_obj:
+        data = fb_obj.readlines()
+    return data
+
 
 def extract_cols(row):
-	rows = row.split(" ")
-	if len(rows) < 10:
-		return False
-	records = []
-	index = 0 
-	while len(records) < 10:
-		try:
-			value = rows[index].strip()
-			index+=1
-			if value == 'Team':
-				break
-			if len(value):
-				records.append(value)
-		except Exception as e:
-			break
-	indices_to_access = [1,6,8]
-	if len(records) == 10:
-		return list(map(records.__getitem__, indices_to_access))
-	
-	return []
+    rows = row.split(" ")
+    if len(rows) < 10:
+        return False
+    records = []
+    index = 0
+    while len(records) < 10:
+        try:
+            value = rows[index].strip()
+            index += 1
+            if value == 'Team':
+                break
+            if len(value):
+                records.append(value)
+        except Exception as e:
+            break
+    indices_to_access = [1, 6, 8]
+    if len(records) == 10:
+        return list(map(records.__getitem__, indices_to_access))
+
+    return []
+
 
 def calculate_min_diff(data_list):
-	max_diff_record = [10000, 0]
-	for row in data_list:
-		cols = extract_cols(row)
-		if not cols:
-			continue
-		diff = abs(int(cols[1]) - int(cols[2]))
-		if diff < max_diff_record[0]:
-			max_diff_record[0] = diff
-			max_diff_record[1] = cols
-	return max_diff_record
+    max_diff_record = [10000, 0]
+    for row in data_list:
+        cols = extract_cols(row)
+        if not cols:
+            continue
+        diff = abs(int(cols[1]) - int(cols[2]))
+        if diff < max_diff_record[0]:
+            max_diff_record[0] = diff
+            max_diff_record[1] = cols
+    return max_diff_record
+
 
 if __name__ == "__main__":
 
-	print(f"Reading the input file {input_file}")
-	data = read_soccer_data(input_file)
-	max_diff_record = calculate_min_diff(data)
-	print(f"Team with smallest difference is {max_diff_record[0]} and Goals for and against are {max_diff_record[1][1]} and {max_diff_record[1][2]}")
-
+    print(f"Reading the input file {input_file}")
+    data = read_soccer_data(input_file)
+    max_diff_record = calculate_min_diff(data)
+    print(
+        f"Team with smallest difference is {max_diff_record[0]} and Goals for and against are {max_diff_record[1][1]} and {max_diff_record[1][2]}")
